89ed84f8c11a1082d1a9e1d5df40e8d28256538d
Miljan <miljancode@gmail.com>
2023-01-07 15:42:14 +0100

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import rays from './assets/rays.png';
+import { BsMoonFill, BsSunFill } from 'react-icons/bs';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={darkMode ? 'dark' : ''}>
+      <div className="dark:bg-gradient-to-b dark:from-slate-800 dark:to-slate-900 dark:text-white min-h-[100vh]">
+        <div className="absolute sm:top-[-20rem] top-[-5rem] left-[50%] translate-x-[-50%] dark:opacity-50 h-[30rem] w-[25rem] xs:h-[40rem] xs:w-[30rem] sm:h-[76rem] sm:w-[59rem] scale-[1.3] opacity-0 z-0">
+          <img src={rays} alt="" height={'100%'} width={'100%'} />
+        </div>
+        <header className="relative p-5 bg-slate-100 dark:bg-slate-900 dark:bg-opacity-20 z-50">
+          <div className="max-w-[120rem] mx-auto flex items-center justify-between">
+            <p className="text-primaryDark font-bold dark:text-white dark:hover:text-primary transition-colors cursor-pointer">
+              miljan.dev
+            </p>
+            <nav>
+              <ul className="flex gap-10 font-medium">
+                <li className="hover:text-primaryDark dark:hover:text-primary cursor-pointer transition-all">
+                  Home
+                </li>
+                <li className="hover:text-primaryDark dark:hover:text-primary cursor-pointer transition-all">
+                  About
+                </li>
+                <li className="hover:text-primaryDark dark:hover:text-primary cursor-pointer transition-all">
+                  Projects
+                </li>
+              </ul>
+            </nav>
+            <div>
+              <button
+                onClick={() => setDarkMode((prev) => !prev)}
+                className="text-black dark:text-white hover:text-primaryDark dark:hover:text-primary cursor-pointer text-3xl transition-all block dark:bg-slate-700 p-3 rounded-full"
+              >
+                {darkMode && <BsSunFill />}
+                {!darkMode && <BsMoonFill />}
+              </button>
+            </div>
+          </div>
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2DD4BF',
+        primaryDark: '#12554c',
+      },
+      fontFamily: {
+        open: ['Open Sans', 'sans-serif'],
+      },
+    },
+    screens: {
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
+    },
+  },
+  plugins: [],
+};
